b6fff9d46fbda2326bad7d2acb4e8e0ff14eb677
zae-hyeong <p0138@naver.com>
2023-12-21 22:27:03 +0900

    [feat] add signup input valid check

--- src/Component/Auth/Login.js
+++ src/Component/Auth/Login.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignup.css";
 
 const Login = () => {
+  const [idInputValue, setIdInputValue] = useState("");
+  const [passwordInputValue, setPasswordInputValue] = useState("");
+
+  const idChangeHandler = (e) => {
+    setIdInputValue(e.target.value);
+  };
+
+  const passwordChangeHandler = (e) => {
+    setPasswordInputValue(e.target.value);
+  };
+
   return (
     <div className="login-wrapper">
       <form id="login-form" action="submit">
-        <label className="login-form-label" htmlFor="login-form">Login</label>
-        <input id="id-input" type="text" placeholder="아이디" />
-        <input id="pw-input" type="password" placeholder="비밀번호" />
+        <label className="login-form-label" htmlFor="login-form">
+          Login
+        </label>
+
+        <input
+          id="id-input"
+          type="text"
+          placeholder="아이디"
+          value={idInputValue}
+          onChange={idChangeHandler}
+        />
+        <input
+          id="pw-input"
+          type="password"
+          placeholder="비밀번호"
+          value={passwordInputValue}
+          onChange={passwordChangeHandler}
+        />
         <div id="login-detail">
           <span className="signup">회원가입</span>
           <div id="find-wrapper">
@@ -16,9 +42,7 @@ const Login = () => {
           </div>
           <div id="save-id-wrapper">
             <input id="save-id" type="checkbox"></input>
-            <label htmlFor="save-id">
-              아이디 저장
-            </label>
+            <label htmlFor="save-id">아이디 저장</label>
           </div>
         </div>
       </form>
--- src/Component/Auth/Signup.js
+++ src/Component/Auth/Signup.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignup.css";
 
 const Signup = () => {
+  const [emailInputValue, setEmailInputValue] = useState("");
+  const [idInputValue, setIdInputValue] = useState("");
+  const [passwordInputValue, setPasswordInputValue] = useState("");
+  const [passwordConfirmInputValue, setPasswordConfirmInputValue] = useState("");
+  const [phoneNumberInputValue, setPhoneNumberInputValue] = useState("");
+  
+  const emailRegex = new RegExp("^([A-Za-z]|[0-9])+$");
+  const phoneNumberRegex = new RegExp("/^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/");
+
+  const emailInputHandler = (e) => {
+    setEmailInputValue(e.target.value);
+    if (!emailRegex.test(e.target.value)) {
+      e.target.className = e.target.className + " deactive";
+    } else {
+      e.target.className = "";
+    }
+  };
+
+  const idInputHandler = (e) => {
+    setIdInputValue(e.target.value);
+    if (e.target.value.length <= 5 && e.target.value.length > 0) {
+      e.target.className = e.target.className + " deactive";
+    } else {
+      e.target.className = "";
+    }
+  };
+
+  const passwordInputHandler = (e) => {
+    setPasswordInputValue(e.target.value);
+    if (e.target.value.length <= 5 && e.target.value.length > 0) {
+      e.target.className = e.target.className + " deactive";
+    } else {
+      e.target.className = "";
+    }
+  };
+
+  const passwordConfirmInputHandler = (e) => {
+    setPasswordConfirmInputValue(e.target.value);
+    if (e.target.value !== passwordInputValue) {
+      e.target.className = e.target.className + " deactive";
+    } else {
+      e.target.className = "";
+    }
+  };
+
+  const phoneNumberInputHandler = (e) => {
+    setPhoneNumberInputValue(e.target.value);
+    console.log(phoneNumberRegex.test(e.target.value));
+    if (!phoneNumberRegex.test(e.target.value)) {
+      e.target.className = e.target.className + " deactive";
+    } else {
+      e.target.className = "";
+    }
+  };
+  
   return (
     <div className="signup-wrapper">
       <form id="signup-form" action="submit">
@@ -9,12 +64,24 @@ const Signup = () => {
           Signup
         </label>
         <div className="signup-input-wrapper">
+          <label htmlFor="email-id-input">이메일</label>
+          <input
+            id="email-input"
+            type="text"
+            placeholder="이메일"
+            value={emailInputValue}
+            onChange={emailInputHandler}
+          />
+        </div>
+        <div className="signup-input-wrapper">
           <label htmlFor="signup-id-input">아이디</label>
           <input
             id="signup-id-input"
             className="essential-input-element"
             type="text"
             placeholder="아이디"
+            value={idInputValue}
+            onChange={idInputHandler}
           />
         </div>
         <div className="signup-input-wrapper">
@@ -24,6 +91,8 @@ const Signup = () => {
             className="essential-input-element"
             type="password"
             placeholder="비밀번호"
+            value={passwordInputValue}
+            onChange={passwordInputHandler}
           />
         </div>
         <div className="signup-input-wrapper">
@@ -33,6 +102,8 @@ const Signup = () => {
             className="essential-input-element"
             type="password"
             placeholder="비밀번호 확인"
+            value={passwordConfirmInputValue}
+            onChange={passwordConfirmInputHandler}
           />
         </div>
         <div className="signup-input-wrapper">
@@ -40,8 +111,10 @@ const Signup = () => {
           <input
             id="signup-phone-number-input"
             className="essential-input-element"
-            type="password"
+            type="text"
             placeholder="000-0000-0000"
+            value={phoneNumberInputValue}
+            onChange={phoneNumberInputHandler}
           />
         </div>
         <div id="signup-detail">
